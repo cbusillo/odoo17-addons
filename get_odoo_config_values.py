1e205c98177273d677e8711afa952d837ee4b7ef
Chris Busillo <info@shinycomputers.com>
2024-04-08 18:30:04 -0400

    use default instead of "False"

--- get_odoo_config_values.py
+++ get_odoo_config_values.py
@@ -2,19 +2,24 @@ import configparser
 import sys
 
 
-def get_odoo_config_values(
-    file_path: str,
-) -> tuple[str, str, str, str, str]:
+def get_odoo_config_values(file_path: str) -> tuple[str, ...]:
     config = configparser.ConfigParser()
     config.read(file_path)
 
-    db_host = config.get("options", "db_host", fallback="localhost")
-    db_port = config.get("options", "db_port", fallback="5432")
-    db_user = config.get("options", "db_user", fallback="odoo")
-    db_password = config.get("options", "db_password", fallback="")
-    db_name = config.get("options", "db_name", fallback="odoo")
+    fallback_values = {
+        "db_host": "localhost",
+        "db_port": "5432",
+        "db_user": "odoo",
+        "db_password": "",
+        "db_name": "odoo",
+    }
 
-    return db_host, db_port, db_name, db_user, db_password
+    config_values = []
+    for key, fallback in fallback_values.items():
+        value = config.get("options", key, fallback=fallback)
+        config_values.append(value if value != "False" else fallback)
+
+    return tuple(config_values)
 
 
 if __name__ == "__main__":
