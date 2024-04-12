047bc1b517fd6f933dbe62214d704258a9296475
Chris Busillo <info@shinycomputers.com>
2024-04-11 20:52:52 -0400

    working hiding and on change

--- product_connect/static/src/js/motor_test_widget.js
+++ product_connect/static/src/js/motor_test_widget.js
@@ -1,115 +1,281 @@
 /** @odoo-module **/
-import { Component, useState } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
-import { registry } from "@web/core/registry";
-import { groupBy, sortBy } from "@web/core/utils/arrays"
-import { BadgeSelectionField } from "@web/views/fields/badge_selection/badge_selection_field";
-import { FloatField } from "@web/views/fields/float/float_field";
-import { CharField } from "@web/views/fields/char/char_field";
-import { BinaryField } from "@web/views/fields/binary/binary_field";
+import { Component, onMounted, onPatched, useState } from '@odoo/owl'
+import { useService } from '@web/core/utils/hooks'
+import { registry } from '@web/core/registry'
+import { groupBy, sortBy } from '@web/core/utils/arrays'
+import {
+  BadgeSelectionField,
+} from '@web/views/fields/badge_selection/badge_selection_field'
+import { FloatField } from '@web/views/fields/float/float_field'
+import { CharField } from '@web/views/fields/char/char_field'
+import { BinaryField } from '@web/views/fields/binary/binary_field'
+import { PdfViewerField } from '@web/views/fields/pdf_viewer/pdf_viewer_field'
 
+/**
+ * @typedef {Object} ConditionalTest
+ * @property {string} data.conditional_test - The name of the conditional test.
+ * @property {string} data.hidden_tests - The type of action for the conditional test.
+ */
+/**
+ * @typedef {Object} MotorTestRecord
+ * @property {string} data.result_type - The type of the result.
+ * @property {Array<ConditionalTest>} data.conditional_tests - The conditional tests for the result.
+ * @property {number} data.section_sequence - The sequence of the section.
+ */
 export class MotorTestWidget extends Component {
-  setup() {
-    this.motorTestsBySection = useState({});
-    this.selectionFieldDomains = useState({});
-    this.orm = useService("orm");
-
-    this.updateMotorTests();
+  static props = {
+    id: String,
+    name: String,
+    record: Object,
+    readonly: Boolean,
   }
 
-  updateMotorTests() {
-    const {name, record} = this.props;
+  async setup() {
+    this.motorTestsBySection = useState({ sections: [] })
+    this.selectionFieldDomains = useState({})
+    this.conditionsById = {}
+    this.allTests = []
+    this.orm = useService('orm')
 
-    const motorTestsRecords = record.data[name].records;
-    const missingParts = record.data.parts.records.filter(part => part.data.missing);
+    onMounted(() => {
+      this.loadMotorTests()
+    })
 
-    // Filter out hidden tests based on missing parts
-    const visibleTests = motorTestsRecords.filter(test => {
-      const hiddenByParts = missingParts.some(part =>
-        part.data.hidden_tests.records.some(hiddenTest =>
-          hiddenTest.data.id === test.data.template[0]
-        )
-      );
-      return !hiddenByParts;
-    });
-
-    // Add or remove tests based on previous test results
-    const conditionalTests = visibleTests.flatMap(test => {
-      const result = test.data[`${test.data.result_type}_result`];
-      return test.data.conditional_tests.records.filter(conditionalTest =>
-        this.evaluateCondition(result, conditionalTest)
-      );
-    });
-
-    const finalTests = [...visibleTests, ...conditionalTests];
-    this.sortMotorTests(finalTests);
-    this.groupMotorTestsBySection(finalTests);
+    // onPatched(() => {
+    //   if (this.shouldReloadTests(this.allTests)) {
+    //     this.loadMotorTests()
+    //   }
+    // })
   }
 
-  evaluateCondition(result, test) {
-    const conditions = test.data.conditions.records;
-    const resultType = test.data.result_type;
+  async onFieldChanged(event) {
+    console.log('Field changed:', event)
+    await this.loadMotorTests()
+  }
 
-    for (const condition of conditions) {
-      const {condition_value: conditionValue, action_type: actionType, conditional_test: conditionalTest} = condition.data;
+  async loadMotorTests() {
+    const { name, record } = this.props
+    this.allTests = record.data[name].records
+    const missingParts = record.data.parts.records.filter(
+      (part) => part.data.missing,
+    )
 
-      let conditionMet = false;
+    const conditionIds = this.allTests.flatMap(
+      (record) => record.data.conditions.currentIds,
+    )
 
-      if (resultType === "selection") {
-        conditionMet = result.toLowerCase() === conditionValue.toLowerCase();
-      } else if (resultType === "numeric") {
-        // Perform numeric comparison based on your requirements
-        // For example, checking if the result is greater than the condition value
-        conditionMet = parseFloat(result) > parseFloat(conditionValue);
-      } else if (resultType === "yes_no") {
-        conditionMet = result.toLowerCase() === conditionValue.toLowerCase();
-      }
+    try {
+      const conditions = await this.orm.searchRead(
+        'motor.test.template.condition',
+        [['id', 'in', conditionIds]],
+        ['action_type', 'condition_value', 'template', 'conditional_test'],
+      )
 
-      if (conditionMet) {
-        const conditionalTestId = conditionalTest.data.id;
-        const conditionalTestInstance = this.env.model.root.data.tests.records.find(
-          (t) => t.data.template.data.id === conditionalTestId
-        );
+      this.conditionsById = Object.fromEntries(
+        conditions.map((condition) => [
+          condition.conditional_test[0],
+          condition,
+        ]),
+      )
 
-        if (conditionalTestInstance) {
-          conditionalTestInstance.data.is_applicable = actionType === "show";
-        }
+      const sortedTests = this.sortMotorTests(this.allTests)
+      console.log('Before update:', this.motorTestsBySection)
+      this.motorTestsBySection.sections = this.groupMotorTestsBySection(
+        sortedTests,
+        missingParts,
+      )
+      console.log('After update:', this.motorTestsBySection)
+    } catch (error) {
+      console.error('Error loading motor tests:', error)
+    }
+  }
+
+  shouldReloadTests(currentTests) {
+    for (const test of currentTests) {
+      if (test._changes && Object.keys(test._changes).length > 0) {
+        return true
       }
     }
   }
 
   sortMotorTests(motorTests) {
-    sortBy(motorTests, ({data: {section_sequence = Infinity, sequence = 0}}) => section_sequence - sequence);
+    return sortBy(motorTests, (test) => [
+      test.data.section_sequence || 0,
+      test.data.sequence || 0,
+    ])
   }
 
-  groupMotorTestsBySection(motorTests) {
-    this.motorTestsBySection = groupBy(motorTests, test => test.data.section[1]);
-    for (const tests of Object.values(this.motorTestsBySection)) {
-      for (const test of tests) {
-        this.setSelectionFieldDomain(test);
+  groupMotorTestsBySection(motorTests, missingParts) {
+    const groupedTests = groupBy(motorTests, (test) => test.data.section[1])
+
+    return Object.entries(groupedTests).reduce((acc, [section, tests]) => {
+      const filteredTests = tests.filter((test) => {
+        const resultType = test.data.result_type
+        const result = test.data[`${test.data.result_type}_result`]
+        const isApplicable = this.evaluateTestApplicability(test, missingParts)
+        if (!isApplicable) {
+          return false
+        }
+
+        const showConditions = test.data.conditions.records.filter(
+          (condition) => condition.data.action_type === 'show',
+        )
+        return showConditions.every((condition) =>
+          this.evaluateCondition(result, resultType, condition),
+        )
+      })
+
+      const conditionalTests = filteredTests.flatMap((test) => {
+        const resultType = test.data.result_type
+        const result = test.data[`${test.data.result_type}_result`]
+
+        return test.data.conditions.records.filter(
+          (conditionalTest) =>
+            conditionalTest.data &&
+            Object.keys(conditionalTest.data).length > 0 &&
+            this.evaluateCondition(result, resultType, conditionalTest),
+        )
+      })
+
+      acc[section] = this.sortMotorTests([
+        ...filteredTests,
+        ...conditionalTests,
+      ])
+
+      for (const test of acc[section]) {
+        this.setSelectionFieldDomain(test)
       }
-    }
+
+      return acc
+    }, {})
   }
 
-  setSelectionFieldDomain({data: {result_type: resultType, selection_options: selectionOptions}, id}) {
+  evaluateTestApplicability(test, missingParts) {
+    // Check if the test is hidden by missing parts
+    const hiddenByParts = missingParts.some((part) =>
+      part.data.hidden_tests.currentIds.includes(test.data.template[0]),
+    )
+    if (hiddenByParts) {
+      console.log('Test hidden by missing parts:', test.data.name)
+      return false
+    }
+
+    const hideConditions = test.data.conditional_tests.records.map(
+      (condition) => {
+        const conditionRecord = Object.values(this.conditionsById).find(
+          (c) => c.id === condition.resId,
+        )
+        if (conditionRecord && conditionRecord.action_type === 'hide') {
+          return conditionRecord
+        }
+        return null
+      }).filter(Boolean)
+
+    const hideConditionsMet = hideConditions.some((condition) => {
+      const templateTest = this.allTests.find(
+        (t) => t.data.template[0] === condition.template[0],
+      )
+      if (templateTest) {
+        const resultType = templateTest.data.result_type
+        const result = templateTest.data[`${resultType}_result`]
+        return this.evaluateCondition(result, resultType, condition)
+      }
+      return false
+    })
+
+    console.log(
+      'Conditions met for test:',
+      test.data.name,
+      'Hide:',
+      hideConditionsMet,
+    )
+
+    if (hideConditionsMet) {
+      return false
+    }
+
+    const showConditions = test.data.conditional_tests.records.map(
+      (condition) => {
+        const conditionRecord = this.conditionsById[condition.data.id]
+        if (conditionRecord && conditionRecord.action_type === 'show') {
+          return conditionRecord
+        }
+        return null
+      }).filter(Boolean)
+
+    const showConditionsMet = showConditions.every((condition) => {
+      const templateTest = this.allTests.find(
+        (t) => t.data.template[0] === condition.template[0],
+      )
+      if (templateTest) {
+        const resultType = templateTest.data.result_type
+        const result = templateTest.data[`${resultType}_result`]
+        return this.evaluateCondition(result, resultType, condition)
+      }
+      return true
+    })
+
+    console.log(
+      'Conditions met for test:',
+      test.data.name,
+      'Show:',
+      showConditionsMet,
+    )
+
+    if (showConditions.length > 0) {
+      return showConditionsMet
+    }
+    return true
+  }
+
+  evaluateCondition(result, resultType, condition) {
+    const conditionRecord = Object.values(this.conditionsById).find(
+      (c) => c.id === condition.id,
+    )
+
+    const { condition_value: conditionValue } = conditionRecord
+
+    let conditionMet = false
+
+    if (resultType === 'selection') {
+      conditionMet = result.toLowerCase() === conditionValue.toLowerCase()
+    } else if (resultType === 'numeric') {
+      conditionMet = parseFloat(result) > parseFloat(conditionValue)
+    } else if (resultType === 'yes_no') {
+      conditionMet = result.toLowerCase() === conditionValue.toLowerCase()
+    }
+    console.log('Condition evaluation:', condition, conditionMet)
+    return conditionMet
+  }
+
+  setSelectionFieldDomain({
+    data: { result_type: resultType, selection_options: selectionOptions },
+    id,
+  }) {
     if (resultType === 'selection') {
       this.selectionFieldDomains[id] = [
         ['id', '=', selectionOptions.currentIds],
-      ];
+      ]
     }
   }
 
   // noinspection JSUnusedGlobalSymbols
   getSelectionFieldDomain(testId) {
-    return this.selectionFieldDomains[testId] || false;
+    return this.selectionFieldDomains[testId] || []
   }
 }
 
-MotorTestWidget.template = "product_connect.MotorTestWidget";
-MotorTestWidget.components = {BadgeSelectionField, FloatField, CharField, BinaryField};
+MotorTestWidget.template = 'product_connect.MotorTestWidget'
+MotorTestWidget.components = {
+  BadgeSelectionField,
+  FloatField,
+  CharField,
+  BinaryField,
+  PdfViewerField,
+}
 
 export const motorTestWidget = {
   component: MotorTestWidget,
-};
+}
 
-registry.category("fields").add("motor_test_widget", motorTestWidget);
+registry.category('fields').add('motor_test_widget', motorTestWidget)
