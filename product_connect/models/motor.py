e9b37479c131b156bcbccea6f5191ff2bb301982
Chris Busillo <info@shinycomputers.com>
2024-04-17 23:11:54 -0400

    add new motor types

--- product_connect/models/motor.py
+++ product_connect/models/motor.py
@@ -219,10 +219,15 @@ class Motor(models.Model):
             ("i2", "Inline 2"),
             ("i3", "Inline 3"),
             ("i4", "Inline 4"),
+            ("i5", "Inline 5"),
             ("i6", "Inline 6"),
+            ("i8", "Inline 8"),
+            ("v2", "V2"),
             ("v4", "V4"),
             ("v6", "V6"),
             ("v8", "V8"),
+            ("v10", "V10"),
+            ("v12", "V12"),
         ]
     )
     model = fields.Char()
