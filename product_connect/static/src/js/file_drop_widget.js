72b022a9a51d5476948fa324ccd20eae80f58219
Chris Busillo <info@shinycomputers.com>
2024-04-21 12:57:45 -0400

    fix whitespace

--- product_connect/static/src/js/file_drop_widget.js
+++ product_connect/static/src/js/file_drop_widget.js
@@ -1,73 +1,73 @@
 /** @odoo-module **/
-const {Component} = owl;
-import { registry } from "@web/core/registry";
-import { binaryField } from "@web/views/fields/binary/binary_field";
+const { Component } = owl
+import { registry } from '@web/core/registry'
+import { binaryField } from '@web/views/fields/binary/binary_field'
 
 export class FileDropWidget extends Component {
   setup() {
-    super.setup();
+    super.setup()
   }
 
   onDrop(ev) {
-    ev.target.classList.add("drag-over");
-    ev.target.textContent = "Drop...";
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.target.classList.add('drag-over')
+    ev.target.textContent = 'Drop...'
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.dataTransfer) {
-      const {files} = ev.dataTransfer;
+      const { files } = ev.dataTransfer
       const sortedFiles = [...files].sort((a, b) =>
         a.name.localeCompare(b.name),
-      );
+      )
       sortedFiles.forEach((file, index) => {
         if (file instanceof Blob) {
-          const reader = new FileReader();
+          const reader = new FileReader()
           reader.onload = async (e) => {
-            const {result} = e.target;
-            const splitResult = result.split(",");
+            const { result } = e.target
+            const splitResult = result.split(',')
             if (splitResult.length > 1) {
-              const baseData = splitResult[1];
+              const baseData = splitResult[1]
               const imageData = {
                 image: baseData,
                 index: index,
-              };
-              console.log(this.props);
-              this.props.record.update({[this.props.name]: imageData});
+              }
+              console.log(this.props)
+              this.props.record.update({ [this.props.name]: imageData })
             } else {
-              console.error("Unable to split result into data and mime type");
+              console.error('Unable to split result into data and mime type')
             }
-          };
-          reader.readAsDataURL(file);
+          }
+          reader.readAsDataURL(file)
         } else {
-          console.error("File is not a Blob");
+          console.error('File is not a Blob')
         }
-      });
+      })
     } else {
-      console.error("dataTransfer is not available");
+      console.error('dataTransfer is not available')
     }
   }
 
   onDragEnter(ev) {
-    ev.preventDefault();
-    ev.target.classList.remove("drag-over");
-    ev.target.textContent = "Release";
+    ev.preventDefault()
+    ev.target.classList.remove('drag-over')
+    ev.target.textContent = 'Release'
   }
 
   onDragLeave(ev) {
-    ev.preventDefault();
-    ev.target.classList.add("drag-over");
-    ev.target.textContent = "Drop...";
+    ev.preventDefault()
+    ev.target.classList.add('drag-over')
+    ev.target.textContent = 'Drop...'
   }
 
   onDragOver(ev) {
-    ev.preventDefault();
+    ev.preventDefault()
   }
 }
 
-FileDropWidget.template = "FileDropWidget";
+FileDropWidget.template = 'FileDropWidget'
 
 export const fileDropWidget = {
   ...binaryField,
   component: FileDropWidget,
-};
+}
 
-registry.category("fields").add("file_drop", fileDropWidget);
+registry.category('fields').add('file_drop', fileDropWidget)
