b14554a2e48f614f5b65221a6bbd4f1a05592c69
Chris Busillo <info@shinycomputers.com>
2024-04-30 17:25:02 -0400

    check for field before alter

--- product_connect/migrations/17.0.2.8/pre-delete_product_import_image.py
+++ product_connect/migrations/17.0.2.8/pre-delete_product_import_image.py
@@ -6,11 +6,18 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, _version) -> None:
     _logger.info(f"Running pre-migration script: {__name__}")
 
-    # noinspection SqlWithoutWhere
-    cr.execute("DELETE FROM product_import_image")
+    cr.execute("""
+        SELECT column_name 
+        FROM information_schema.columns 
+        WHERE table_name='product_import_image' AND column_name='product'
+    """)
+    result = cr.fetchone()
 
-    # noinspection SqlResolve
-    cr.execute("ALTER TABLE product_import_image RENAME COLUMN product to product_id")
+    if result:
+        # noinspection SqlResolve
+        cr.execute("ALTER TABLE product_import_image RENAME COLUMN product to product_id")
+
+    cr.execute("DELETE FROM product_import_image WHERE product_id IS NULL")
 
     deleted_count = cr.rowcount
     _logger.info("Deleted %d records from product_import_image table", deleted_count)
