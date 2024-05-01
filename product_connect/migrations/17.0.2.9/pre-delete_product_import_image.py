dfd60877c7c1107113d6d4f08cddea5c00dae316
Chris Busillo <info@shinycomputers.com>
2024-05-01 14:13:49 -0400

    fixed logging of delete records

--- product_connect/migrations/17.0.2.9/pre-delete_product_import_image.py
+++ product_connect/migrations/17.0.2.9/pre-delete_product_import_image.py
@@ -9,6 +9,7 @@ def migrate(cr, version) -> None:
     _logger.info("Running post-migration script.")
 
     cr.execute("DELETE FROM product_import_image WHERE product_id IS NULL")
+    _logger.info(f"Deleted {cr.rowcount} records with NULL 'product_id' values.")
     rename_field(cr, "product.import.image", "product_id", "product")
 
     cr.connection.commit()
