78f74d4422e4cbe4145a4f9718dd037568e8d2a7
Chris Busillo <info@shinycomputers.com>
2024-05-07 17:22:28 -0400

    migrations

---
+++ product_connect/migrations/17.0.3.1/post-update_motor_motor_product_and_product_import_images.py
@@ -0,0 +1,25 @@
+import logging
+
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+MODELS = ["motor.image", 'motor.product.image', "product.import.image"]
+
+
+def migrate(cr, version) -> None:
+    env = util.env(cr)
+
+    for model_name in MODELS:
+        model = env[model_name]
+        parent_model_name = model_name.rsplit('.', 1)[0]
+        if parent_model_name:
+            parent_model = env[parent_model_name]
+            if 'icon' in parent_model._fields:
+                records = parent_model.search([])
+
+                records._compute_icon()
+
+        records = model.search([])
+        for record in records:
+            record.image_1920 = record.image_1920
---
+++ product_connect/migrations/17.0.3.1/pre-rename_product_import_image_field.py
@@ -0,0 +1,14 @@
+import logging
+
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, _version) -> None:
+    _logger.info(f"Running migration script: {__name__}")
+
+    util.rename_field(cr, "product.import.image", "image_data", "image_1920")
+    util.remove_field(cr, "product.import.image", "image_data")
+
+    _logger.info("Migration script completed")
