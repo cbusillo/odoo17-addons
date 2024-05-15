ac304aad66a151e2356dc784cf300dfe1b205fb7
Chris Busillo <info@shinycomputers.com>
2024-05-15 19:44:33 -0400

    working with html5qrcode

--- product_connect/static/src/js/widgets/qr_code_widget.js
+++ product_connect/static/src/js/widgets/qr_code_widget.js
@@ -1,5 +1,5 @@
 /** @odoo-module **/
-import { onMounted, onWillUnmount, useState } from '@odoo/owl';
+import { onMounted, onWillUnmount, useRef, useState } from '@odoo/owl';
 import { ConfirmationDialog } from '@web/core/confirmation_dialog/confirmation_dialog';
 import { CharField, charField } from '@web/views/fields/char/char_field';
 import { registry } from '@web/core/registry';
@@ -16,11 +16,13 @@ class QRCodeWidget extends CharField {
             scanEnabled: true,
             buttonLabel: 'Stop',
         })
+        this.qrReaderRef = useRef('qrReader');
+
 
         this.onScanSuccess = this.onScanSuccess.bind(this);
         onMounted(() => {
             // noinspection JSUnresolvedReference
-            this.htmlQrCode = new window.Html5Qrcode("reader");
+            this.htmlQrCode = new window.Html5Qrcode("qrReader");
             this.startScanner();
         })
 
@@ -44,29 +46,54 @@ class QRCodeWidget extends CharField {
                             this.startScanner();
                         },
                     });
-                });
+                })
         } catch (error) {
             console.error('An error occurred:', error)
         }
     }
 
     startScanner() {
-        const config = {
-            fps: 10,
-            qrbox: { width: 200, height: 200 },
-        };
-        this.htmlQrCode.start(
-            { facingMode: "environment" },
-            config,
-            this.onScanSuccess
-        );
+        this.qrReaderRef.el.classList.remove('d-none');
+        const currentState = this.htmlQrCode.getState()
+        if (currentState === Html5QrcodeScannerState.PAUSED) {
+            this.htmlQrCode.resume()
+        } else {
+            this.htmlQrCode.start(
+                { facingMode: "environment" },
+                {
+                    fps: 60,
+                },
+                this.onScanSuccess
+            )
+            // const scannerCapabilities = this.htmlQrCode.getRunningTrackCapabilities();
+            // const zoomCapability = scannerCapabilities.zoom > 2 ? 2.0 : scannerCapabilities.zoom;
+            //
+            // const constraints = {
+            //     // Set a higher height then default to get a better resolution
+            //     width: { ideal: 2000 },
+            //     height: { ideal: 2000 },
+            //
+            //     // Set the ideal frame rate based on the capabilities
+            //     frameRate: { ideal: scannerCapabilities.frameRate?.max || 30 },
+            //
+            //     advanced: [
+            //         // conditionally add the zoom capability if it is supported
+            //         ...(scannerCapabilities.zoom ? [{ zoom: zoomCapability }] : []),
+            //
+            //         // conditionally add the focus distance capability if it is supported
+            //         ...(scannerCapabilities.focusDistance ? [{ focusDistance: 1 }] : []),
+            //     ],
+            // };
+            //
+            //
+            // await this.htmlQrCode.applyVideoConstraints(constraints);
+        }
         this.state.buttonLabel = 'Stop'
     }
 
     stopScanner() {
-        this.htmlQrCode?.stop().catch((error) => {
-            console.error('Failed to stop scanner:', error);
-        });
+        this.qrReaderRef.el.classList.add('d-none');
+        this.htmlQrCode?.pause(true)
         this.state.buttonLabel = 'Scan'
     }
 
@@ -78,9 +105,15 @@ class QRCodeWidget extends CharField {
                 this.stopScanner()
             }
         } else {
-            this.startScanner()
+            this.startScanner().catch((error) => {
+                console.error('An error occurred:', error)
+            })
         }
         this.state.scanEnabled = !this.state.scanEnabled;
+    }
+
+    onInputFocus() {
+        this.startScanner()
     }
 }
 
