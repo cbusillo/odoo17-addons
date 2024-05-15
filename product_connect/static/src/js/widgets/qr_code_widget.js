de94514c28aad723a19d8cc1a4c272390b892de8
Chris Busillo <info@shinycomputers.com>
2024-05-15 00:53:50 -0400

    Initial working

--- product_connect/static/src/js/widgets/qr_code_widget.js
+++ product_connect/static/src/js/widgets/qr_code_widget.js
@@ -4,59 +4,36 @@ import { CharField } from '@web/views/fields/char/char_field';
 import { registry } from '@web/core/registry';
 import { useRef } from '@odoo/owl';
 
+const QrScanner = window.QrScanner;
+
 class QRCodeWidget extends CharField {
     setup() {
         super.setup();
         this.inputRef = useRef("input");
+        this.videoContainerRef = useRef("videoContainer");
     }
 
-    async scanQRCode() {
-        try {
-            console.log('Requesting camera access...');
-            const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-            console.log('Camera access granted.');
+    scanQRCode() {
+        const videoElement = document.createElement('video');
+        videoElement.setAttribute('playsinline', 'true'); // Required to tell iOS Safari we don't want fullscreen
+        videoElement.style.width = '100%';
+        videoElement.style.height = 'auto';
 
-            const videoElement = document.createElement('video');
-            videoElement.srcObject = stream;
-            videoElement.setAttribute('playsinline', 'true');
-            videoElement.style.position = 'absolute';
-            videoElement.style.top = '0';
-            videoElement.style.left = '0';
-            videoElement.style.width = '100vw';
-            videoElement.style.height = '100vh';
-            videoElement.style.zIndex = '1000'; // Ensure it is on top
-            videoElement.style.background = 'black';
 
-            document.body.appendChild(videoElement);
-            console.log('Video element added to DOM.');
+        this.videoContainerRef.el.appendChild(videoElement);
 
-            await videoElement.play();
-            console.log('Video element playing.');
-
-            const qrScanner = new window.QrScanner(videoElement, result => this.onQRCodeScanned(result));
-            qrScanner.start();
-            console.log('QR Scanner started.');
-
-            this.qrScanner = qrScanner;
-            this.videoElement = videoElement;
-
-        } catch (error) {
-            console.error('Error accessing camera', error);
-        }
-    }
-
-    onQRCodeScanned(result) {
-        console.log('QR Code scanned:', result);
-        this.inputRef.el.value = result;
-        if (this.qrScanner) {
-            this.qrScanner.stop();
-        }
-        if (this.videoElement) {
-            this.videoElement.srcObject.getTracks().forEach(track => track.stop());
-            document.body.removeChild(this.videoElement);
-        }
+        const qrScanner = new QrScanner(
+            videoElement,
+            result => {
+                this.inputRef.el.value = result
+                qrScanner.stop()
+                this.videoContainerRef.el.innerHTML = '';
+            })
+        qrScanner.start()
     }
 }
+
+QRCodeWidget.template = 'product_connect.QRCodeWidget';
 
 export const qrCodeWidget = {
     ...CharField,
