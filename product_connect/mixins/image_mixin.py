cacc0bb4517d1fdace0d0c1ab2518f78fee8d445
Chris Busillo <info@shinycomputers.com>
2024-07-24 20:09:13 -0400

    Rename file_size to image_1920_file_size and update related logic.
    Removed pre-migration script, updated views to reflect the new field name, and modified the image mixin to handle attachment-related file size. This change ensures consistency across the codebase and improves data handling for product images.

--- product_connect/mixins/image_mixin.py
+++ product_connect/mixins/image_mixin.py
@@ -5,17 +5,28 @@ class ImageMixin(models.AbstractModel):
     _description = "Image Mixin"
     _inherit = "image.mixin"
 
-    file_size = fields.Integer(compute="_compute_file_size", store=True, group_operator="sum")
+    attachment = fields.Many2one(
+        "ir.attachment", compute="_compute_attachment", store=True
+    )
+    image_1920_file_size = fields.Integer(related="attachment.file_size")
 
-    @api.depends("image_1920")
-    def _compute_file_size(self) -> None:
-        for record in self:
-            record.file_size = len(record.image_1920 or b'')
+    @api.depends("attachment")
+    def _compute_attachment(self) -> None:
+        IrAttachment = self.env["ir.attachment"]
+        for image in self:
+            image.attachment = IrAttachment.search(
+                [
+                    ("res_model", "=", self._name),
+                    ("res_id", "=", image.id),
+                    ("res_field", "=", "image_1920"),
+                ],
+                limit=1,
+            )
 
     def action_open_full_image(self) -> dict:
         self.ensure_one()
         return {
-            'type': 'ir.actions.act_url',
-            'url': '/web/image?model=product.image&id=%s&field=image_1920' % self.id,
-            'target': 'new',
+            "type": "ir.actions.act_url",
+            "url": f"/web/image?model={self._name}&id=%s&field=image_1920{self.id}",
+            "target": "new",
         }
